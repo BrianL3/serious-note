2879172391268c34021d0afe9ff59f366b57b46e
BrianL3 <Brian.James.Ledbetter@gmail.com>
2015-02-23 09:33:38 -0800

    first commit!

---
+++ Serious Note/Serious Note/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Serious Note
+//
+//  Created by Brian Ledbetter on 2/23/15.
+//  Copyright (c) 2015 Brian Ledbetter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
