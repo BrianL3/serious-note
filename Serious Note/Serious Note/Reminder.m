83ae2e78d8206d7cdf475aeb1e51f9d7696380ef
BrianL3 <Brian.James.Ledbetter@gmail.com>
2015-02-25 11:03:31 -0800

    created some init methods for Reminders

--- Serious Note/Serious Note/Reminder.m
+++ Serious Note/Serious Note/Reminder.m
@@ -10,8 +10,25 @@
 
 @implementation Reminder
 
-//create a Reminder
+//create a Reminder, the standard init.  to be used when creating a new reminder with a unique ID.
+- (instancetype)init{
+    self = [super init];
+    if (self) {
+        //set reminderID as the current clocktime
+        NSDate* currentTime = [NSDate date];
+        self.reminderID = currentTime.timeIntervalSinceReferenceDate;
+    }
+    return self;
+}
 
 //serial a JSON blob into a reminder
+- (instancetype)initWithJSON: (NSDictionary*)jsonDictionary{
+    self = [super init];
+    if (self) {
+        // JSON serialize all the shit here
+    }
+    return self;
+}
+
 
 @end
