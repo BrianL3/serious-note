dc31f9ae84881cadc7dd8cd19e8517918da9997e
John Leonard <cwnidog1@gmail.com>
2015-02-26 09:07:48 -0800

    Adding segue from AddReminer's doneBUtton to SelectRecipient VC

--- Serious Note/Serious Note/Reminder.m
+++ Serious Note/Serious Note/Reminder.m
@@ -39,6 +39,36 @@
     return self;
 }
 
+//create a location-based Reminder, to be used when creating a new reminder with a unique ID.
+- (instancetype)initWithLocation: (CLLocation*) reminderLocation withText:(NSString*)text withAudio:(NSData*)audioData withVideo:(NSData*)videoData
+{
+  self = [super init];
+  if (self) {
+    self.messageType = locationBased;
+    //set reminderID as the current clocktime
+    NSDate* currentTime = [NSDate date];
+    self.reminderID = currentTime.timeIntervalSinceReferenceDate;
+    //PLACEHOLDER: REPLACE THIS!
+    self.userID = @"jim";
+    if (audioData) {
+      self.audioContent = audioData;
+      self.mediaType = audioType;
+    }
+    if (text) {
+      self.textContent = text;
+      self.mediaType = textType;
+    }
+    if (videoData) {
+      self.videoContent = videoData;
+      self.mediaType = videoType;
+    }
+    
+  } // if self
+  NSLog(@"A reminder object was just created with type: %d", self.mediaType);
+  return self;
+} // initWithLocation()
+
+
 //serial a JSON blob into a reminder
 - (instancetype)initWithJSON: (NSDictionary*)jsonDictionary{
     self = [super init];
